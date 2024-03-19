df8d4678b3c9962df68696441235f997782411b5
Dev-RMV <71718060+Dev-RMV@users.noreply.github.com>
2024-03-19 13:22:22 -0300

    Correção das funções remover e atualizar tarefas
    Corrigido o retorno e feito update das DocStrings. Ainda não está testado.

--- atualizar.py
+++ atualizar.py
@@ -1,21 +1,20 @@
 def atualizar_tarefa(lista,id,nome="",descricao="",data_inicio="",data_conclusao="",concluida=""):
     """
     Recebe obrigatoriamente: lista,id e um ou mais parâmentros opcionais.\n
-    Parâmetros opcionais: nome,descricao,data_inicio,data_conclusao,concluida.\n
-    Retorna um booleano que descreve o sucesso da operação (True/False).\n
-    Função que atualiza uma tarefa da lista. Retorna FALSE se nenhuma atualização for passada como parâmetro opcional ou o id não constar na lista.
+    Parâmetros opcionais: nome,descricao,status,prazo_final,urgencia.\n
+    Retorna a lista com a tarefa atualizada ou sem alterações se a  operação falhar.\n
+    Função que atualiza uma tarefa da lista.\n
     """
     
     if nome=="" and descricao=="" and data_inicio=="" and data_conclusao=="" and concluida=="":
-        return False
+        return lista
         
     for n in lista:
-        if n["id"]==id:
+        if int(n["id"])==int(id):
             if nome!="": n["nome"]=nome
             if descricao!="": n["descricao"]=descricao
             if data_inicio!="": n["data_inicio"]=data_inicio
             if data_conclusao!="": n["data_conclusao"]=data_conclusao
             if concluida!="": n["concluida"]=concluida
-            return True
-    
-    return False
+            return lista    
+    return lista
--- remover_tarefa.py
+++ remover_tarefa.py
@@ -1,12 +1,12 @@
 def remover_tarefa(lista,id):
-  """
-  Recebe a lista a manipular e o id da tarefa a remover\n
-  Retorna um booleano que descreve o sucesso da operação (True/False).\n
-  Função que remove uma tarefa da lista. Retorna FALSE se a lista estiver vazia ou o id não constar na lista.
-  """
-  if lista:
-      for n in lista:
-          if n["id"]==id:
-              lista.remove(n)
-              return True
-  return False
+    """
+    Recebe obrigatoriamente: lista,id.\n
+    Retorna a lista com a lista atualizada ou sem alterações se a  operação falhar.\n
+    Função que remove uma tarefa da lista.
+    """
+    if lista:
+        for n in lista:
+            if int(n["id"])==int(id):
+                lista.remove(n)
+                return lista
+    return lista
